d78df97aeaee476e2085713a8122834af1036457
Umberto Toniolo <amountainram@gmail.com>
2023-02-17 09:20:59 +0100

    set default env script

--- scripts/set-default-env.sh
+++ scripts/set-default-env.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-export WORKING_DIR=`pwd`/mocks
+WORKING_DIR=`pwd`
+ENV_DIR=$WORKING_DIR/.env
+mkdir -p $ENV_DIR/public
+mkdir -p $ENV_DIR/configurations
 
-sed -i -r "s#^(RESOURCES_DIRECTORY_PATH=).*#\1$WORKING_DIR#" default.env
+if [[ ! -e $ENV_DIR/config.json ]]; then
+  echo '{}' > $ENV_DIR/config.json
+fi
+
+if [[ ! -e $WORKING_DIR/.env.local ]]; then
+  cp default.env .env.local
+  echo "PUBLIC_DIRECTORY_PATH=$ENV_DIR/public" >> $WORKING_DIR/.env.local
+  echo "RESOURCES_DIRECTORY_PATH=$ENV_DIR/configurations" >> $WORKING_DIR/.env.local
+  echo "SERVICE_CONFIG_PATH=$ENV_DIR/config.json" >> $WORKING_DIR/.env.local
+fi
